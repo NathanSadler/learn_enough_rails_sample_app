0315d17ff819f83f8f2f66b12a7866acb6ace746
Nathan Sadler <nathan@sadlerville.com>
2021-07-01 16:26:41 -0400

    fix the title stuff

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 require 'pry'
 
 RSpec.describe "StaticPages", type: :request do
+  let (:base_title) {"Ruby on Rails Tutorial Sample App"}
+
   describe "GET /home" do
     it "returns http success" do
       get "/static_pages/home"
       expect(response).to have_http_status(:success)
-      binding.pry
+      expect(response.body.include?("<html><head><title>Home | #{base_title}")).to(eq(true))
     end
   end
 
@@ -14,6 +16,7 @@ RSpec.describe "StaticPages", type: :request do
     it "returns http success" do
       get "/static_pages/help"
       expect(response).to have_http_status(:success)
+      expect(response.body.include?("<html><head><title>Help | #{base_title}")).to(eq(true))
     end
   end
 
@@ -21,6 +24,7 @@ RSpec.describe "StaticPages", type: :request do
     it("should get about") do
       get "/static_pages/about"
       expect(response).to(have_http_status(:success))
+      expect(response.body.include?("<html><head><title>About | #{base_title}")).to(eq(true))
     end
   end
 
